04479bcac633eeaf387a7e22ea61c41de228ebfd
seojunmyeong <50089301+seojunmyeong@users.noreply.github.com>
2020-05-22 16:20:52 +0900

    a

--- app/src/main/java/tutorials/crunchify/educationalapp/MainActivity.java
+++ app/src/main/java/tutorials/crunchify/educationalapp/MainActivity.java
@@ -1,8 +1,14 @@
 package tutorials.crunchify.educationalapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        PreferenceManager.setDefaultValues(this, R.xml.preferences,false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void handlerGame (View view){
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    public void handlerScore (View view){
+        Intent intent = new Intent(this, ScoreActivity.class);
+        startActivity(intent);
     }
 }
